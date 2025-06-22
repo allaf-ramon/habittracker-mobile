5bf1b1be5396a9fe03c75d4a2dc8081733bb3d74
Allaf Ramon <allafr1999@gmail.com>
2025-06-22 14:29:21 -0300

    feat: adiciona classe HabitListViewModel para gerenciar lista de habitos

---
+++ app/src/main/java/br/com/habittracker/mobile/viewmodel/HabitListViewModel.java
@@ -0,0 +1,23 @@
+package br.com.habittracker.mobile.viewmodel;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.ViewModel;
+
+import java.util.List;
+
+import br.com.habittracker.mobile.data.model.HabitResponse;
+import br.com.habittracker.mobile.data.repository.HabitRepository;
+
+public class HabitListViewModel extends ViewModel {
+    private HabitRepository repository;
+    private LiveData<List<HabitResponse>> allHabits;
+
+    public HabitListViewModel() {
+        repository = new HabitRepository();
+        allHabits = repository.getAllHabits();
+    }
+
+    public LiveData<List<HabitResponse>> getAllHabits() {
+        return allHabits;
+    }
+}

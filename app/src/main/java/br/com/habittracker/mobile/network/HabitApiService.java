3d44580836d1dc451114195a20b218253ecdad99
Allaf Ramon <allafr1999@gmail.com>
2025-06-23 19:55:55 -0300

    feat: adiciona endpoints para marcar habitos como completos e nao completos

--- app/src/main/java/br/com/habittracker/mobile/network/HabitApiService.java
+++ app/src/main/java/br/com/habittracker/mobile/network/HabitApiService.java
@@ -6,8 +6,10 @@ import br.com.habittracker.mobile.data.model.HabitRequest;
 import br.com.habittracker.mobile.data.model.HabitResponse;
 import retrofit2.Call;
 import retrofit2.http.Body;
+import retrofit2.http.DELETE;
 import retrofit2.http.GET;
 import retrofit2.http.POST;
+import retrofit2.http.Path;
 
 public interface HabitApiService {
     @GET("v1/habits")
@@ -15,4 +17,10 @@ public interface HabitApiService {
 
     @POST("v1/habits")
     Call<HabitResponse> createHabit(@Body HabitRequest habitRequest);
+
+    @POST("v1/habits/{habitId}/completions/{date}")
+    Call<Void> markAsCompleted(@Path("habitId") Long habitId, @Path("date") String date);
+
+    @DELETE("v1/habits/{habitId}/completions/{date}")
+    Call<Void> markAsNotCompleted(@Path("habitId") Long habitId, @Path("date") String date);
 }

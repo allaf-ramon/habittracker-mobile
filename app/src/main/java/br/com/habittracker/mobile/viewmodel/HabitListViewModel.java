5732915bdf856058b201561495f170d1ea769dc0
Allaf Ramon <allafr1999@gmail.com>
2025-06-23 20:17:04 -0300

    feat: implementa metodo para alternar estado de completude dos habitos

--- app/src/main/java/br/com/habittracker/mobile/viewmodel/HabitListViewModel.java
+++ app/src/main/java/br/com/habittracker/mobile/viewmodel/HabitListViewModel.java
@@ -1,20 +1,25 @@
 package br.com.habittracker.mobile.viewmodel;
 
 import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
 import androidx.lifecycle.ViewModel;
 
+import java.text.SimpleDateFormat;
+import java.util.Calendar;
 import java.util.List;
 
 import br.com.habittracker.mobile.data.model.HabitResponse;
 import br.com.habittracker.mobile.data.repository.HabitRepository;
 
 public class HabitListViewModel extends ViewModel {
-    private HabitRepository repository;
-    private LiveData<List<HabitResponse>> allHabits;
+    private final HabitRepository repository;
+    private final MutableLiveData<List<HabitResponse>> allHabits;
+    public final MutableLiveData<Boolean> toggleSuccess = new MutableLiveData<>();
 
     public HabitListViewModel() {
         repository = new HabitRepository();
-        allHabits = repository.getAllHabits();
+        allHabits = new MutableLiveData<>();
+        refreshHabits(); // Carga inicial
     }
 
     public LiveData<List<HabitResponse>> getAllHabits() {
@@ -22,6 +27,19 @@ public class HabitListViewModel extends ViewModel {
     }
 
     public void refreshHabits() {
-        allHabits = repository.getAllHabits();
+        // Usa o LiveData retornado pelo repositório para atualizar 'allHabits'
+        repository.getAllHabits().observeForever(habits -> allHabits.setValue(habits));
+    }
+
+    public void toggleCompletion(HabitResponse habit) {
+        SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
+        String dateString = dateFormat.format(Calendar.getInstance().getTime());
+
+        // O 'toggleSuccess' vai notificar a View (MainActivity) que a operação terminou.
+        if (habit.isCompletedToday()) {
+            repository.markAsNotCompleted(habit.getId(), dateString, toggleSuccess);
+        } else {
+            repository.markAsCompleted(habit.getId(), dateString, toggleSuccess);
+        }
     }
 }

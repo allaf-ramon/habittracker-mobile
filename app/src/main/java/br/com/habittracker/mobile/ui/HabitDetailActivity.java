68097cd6b908f9560affec12ec129dbdeea26821
Allaf Ramon <allafr1999@gmail.com>
2025-06-24 16:04:40 -0300

    feat: adiciona funcionalidade de edicao de habito na tela de detalhes

--- app/src/main/java/br/com/habittracker/mobile/ui/HabitDetailActivity.java
+++ app/src/main/java/br/com/habittracker/mobile/ui/HabitDetailActivity.java
@@ -1,10 +1,14 @@
 package br.com.habittracker.mobile.ui;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -18,6 +22,7 @@ public class HabitDetailActivity extends AppCompatActivity {
     public static final String EXTRA_HABIT_ID = "br.com.habittracker.mobile.HABIT_ID";
     private HabitDetailViewModel viewModel;
     private TextView textName, textDescription, textStreak;
+    private long habitId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,8 +39,8 @@ public class HabitDetailActivity extends AppCompatActivity {
         textDescription = findViewById(R.id.textview_detail_habit_description);
         textStreak = findViewById(R.id.textview_detail_current_streak);
 
-        long habitId = getIntent().getLongExtra(EXTRA_HABIT_ID, -1);
-        if (habitId == -1) {
+        this.habitId = getIntent().getLongExtra(EXTRA_HABIT_ID, -1);
+        if (this.habitId == -1) {
             Toast.makeText(this, "Erro: ID do Hábito não encontrado", Toast.LENGTH_SHORT).show();
             finish();
             return;
@@ -44,7 +49,7 @@ public class HabitDetailActivity extends AppCompatActivity {
         viewModel = new ViewModelProvider(this).get(HabitDetailViewModel.class);
 
         // Observa e atualiza os detalhes do hábito
-        viewModel.getHabitDetails(habitId).observe(this, habit -> {
+        viewModel.getHabitDetails(this.habitId).observe(this, habit -> {
             if (habit != null) {
                 textName.setText(habit.getName());
                 textDescription.setText(habit.getDescription());
@@ -52,10 +57,28 @@ public class HabitDetailActivity extends AppCompatActivity {
         });
 
         // Observa e atualiza as estatísticas
-        viewModel.getHabitStats(habitId).observe(this, stats -> {
+        viewModel.getHabitStats(this.habitId).observe(this, stats -> {
             if (stats != null) {
                 textStreak.setText(String.valueOf(stats.getCurrentStreak()));
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_detail, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if (item.getItemId() == R.id.action_edit_habit) {
+            Intent intent = new Intent(this, AddEditHabitActivity.class);
+            // Passa o ID para a activity saber que está em modo de edição
+            intent.putExtra(EXTRA_HABIT_ID, this.habitId);
+            startActivity(intent);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 }
